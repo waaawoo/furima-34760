c3b8ce29a48d398eb6032f008cbbe4797c6db3d6
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-02-27 12:22:50 +0900

    Basic準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数が指定される
+      # vim ~/.zshrc
+      # source ~/.zshrc
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
