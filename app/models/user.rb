1d3345378954ae932dfa18da0dc65b1b1aabef86
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-01 11:44:44 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :nickname, presence: true
+  validates :f_name, presence: true
+  validates :l_name, presence: true
+  validates :f_name_kana, presence: true
+  validates :l_name_kana, presence: true
+  validates :birthday, presence: true
 end
