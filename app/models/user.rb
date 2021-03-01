6943730be224d6a8990922bd02793b7de093c489
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-01 17:06:27 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # バリデーション
+  # バリデーション 6以上半角英数字混合必須
+  validates :password, length:{minimum: 6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  # から判定
   validates :name, presence: true
-  validates :f_name, presence: true
-  validates :l_name, presence: true
-  validates :f_name_kana, presence: true
-  validates :l_name_kana, presence: true
+  # 全角のみ
+  validates :f_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  # 全角のみ
+  validates :l_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  # 全角カタカタ
+  validates :f_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  # 全角カタカタ
+  validates :f_name_kana, presence: true,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
+  # から判定
   validates :birthday, presence: true
 end
