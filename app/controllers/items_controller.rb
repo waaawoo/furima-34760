d20310001664e4f77f44f973950c5671fd238508
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-02 19:08:20 +0900

    アイテム登録（コントローラ）記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,29 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def crate
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :text,
+      :image,
+      :category_id,
+      :condition_id,
+      :delivery_burden_id,
+      :area_id,
+      :delivery_time_id,
+      :price
+      ).merge(user_id: current_user.id)
+  end
 end
