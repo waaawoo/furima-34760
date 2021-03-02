cc21a0a54c3482017ef3bcab040208e02ab3a91d
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-02 14:39:31 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  # バリデーション
+  # から判定
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+    validates :category_id
+    validates :delivery_burden_id
+    validates :area_id
+    validates :delivery_time_id
+  end
 end
