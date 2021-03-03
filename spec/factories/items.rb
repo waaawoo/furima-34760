1e96c1d89e73f6c888f8423e0e04b262df701fb1
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-03 16:50:09 +0900

    商品モデル単体テスト実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  { Faker::Name.initials }
+    text                  { Faker::Lorem.sentence }
+    price                 { '300' }
+    category_id           { '2' }
+    condition_id          { '2' }
+    delivery_burden_id    { '2' }
+    area_id               { '2' }
+    delivery_time_id      { '2' }
+
+    # アソシエーション
+    association :user
+
+    # ダミー画像
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
