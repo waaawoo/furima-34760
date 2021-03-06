2fd01a8cacee280d45cddf6e366645b415e8c28b
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-06 10:21:39 +0900

    ビューへアイテム情報を表示

--- app/controllers/buyer_history_controller.rb
+++ app/controllers/buyer_history_controller.rb
@@ -1,2 +1,17 @@
 class BuyerHistoryController < ApplicationController
+  before_action :authenticate_user!, only: %i[index]
+  before_action :item_id, only: %i[index]
+
+
+  def index
+  end
+
+  def new
+
+  end
+  private
+
+  def item_id
+    @item = Item.find(params[:item_id])
+  end
 end
