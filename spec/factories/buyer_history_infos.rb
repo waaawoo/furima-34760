fbce29e9bff9c6b8d10409a54803934176d1c86c
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-03-09 00:18:45 +0900

    モデル単体テスト実装（buyer_history_info）

---
+++ spec/factories/buyer_history_infos.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :buyer_history_info do
+    post_num          { "123-4567" }
+    area_id           { 2 }
+    municipalities    { "う市" }
+    address           { "アドレス" }
+    phone_num         { "12345678910" }
+    token             { "token" }
+    user_id           { 1 }
+    item_id           { 1 }
+
+  end
+end
